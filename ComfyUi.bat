97ae28e79eea3210a77777be4aa4c2bc9814d971
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2025-08-08 02:48:15 +0900

    ファイルサイズ上限を上げる。

--- ComfyUi.bat
+++ ComfyUi.bat
@@ -1,4 +1,4 @@
 @echo off
 chcp 65001 > NUL
 
-call %~dp0ComfyUi_NoArgs.bat --auto-launch --fast --use-sage-attention %*
+call %~dp0ComfyUi_NoArgs.bat --auto-launch --fast  --max-upload-size 500 --use-sage-attention %*
