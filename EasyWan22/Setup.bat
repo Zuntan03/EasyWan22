049a5370273b1428fe1262c12d6737b62c338583
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2025-08-13 19:08:29 +0900

    Sigmas を無効化できるようにする Use Sigmas を追加しました。 #12

--- EasyWan22/Setup.bat
+++ EasyWan22/Setup.bat
@@ -50,14 +50,11 @@ if %ERRORLEVEL% neq 0 ( pause & popd & exit /b 1 )
 popd rem "%~dp0.."
 pushd "%~dp0..\ComfyUI\custom_nodes"
 
-@REM 08/07 ab06ac2f646a7e5eb46b022c114a53bcac85009f
-@REM 08/08 a64d8bb0df21efdd933692f11231dbd4abc8d2dc
-@REM 08/09 53e740a1e64e56ab840e3b67127e54de370b416b
-@REM 08/10 7e474b6b16a460112cf3abf12d97bb76730b3203
 @REM 08/11 d6270ace0aac92a39f67fad202b96b94ca67e867
 @REM 08/12 97da701379ca54cef928a469f993601cfcfcb632
+@REM 08/13 d59690105518ab5eef50690028a7b0fe0d793631
 @REM https://github.com/kijai/ComfyUI-WanVideoWrapper/commits/main/
-call :GITHUB_HASH_REQUIREMENTS kijai ComfyUI-WanVideoWrapper main 97da701379ca54cef928a469f993601cfcfcb632
+call :GITHUB_HASH_REQUIREMENTS kijai ComfyUI-WanVideoWrapper main d59690105518ab5eef50690028a7b0fe0d793631
 if %ERRORLEVEL% neq 0 ( popd & exit /b 1 )
 
 xcopy /SQY ComfyUI-WanVideoWrapper\example_workflows\*.* ..\user\default\workflows\Kijai\
